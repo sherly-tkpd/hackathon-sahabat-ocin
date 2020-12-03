6ab48b3e6a570bab088bce947fe4c4f247e2fe18
fandok <mr.parker0@gmail.com>
2020-12-03 15:14:52 +0700

    improve 3d viewer

--- src/components/Viewer3D/index.js
+++ src/components/Viewer3D/index.js
@@ -1,21 +1,14 @@
 import React from "react";
-import { OBJModel, DAEModel } from "react-3d-viewer";
-import freedom from "../../assets/laptop/freedom.obj";
+import { DAEModel } from "react-3d-viewer";
 import alienware from "../../assets/laptop/alienware.dae";
+import "./index.css";
 
 const Viewer3D = () => (
-  <div style={{ display: "flex" }}>
-    <OBJModel
-      width="400"
-      height="400"
-      position={{ x: 0, y: -100, z: 0 }}
-      src={freedom}
-    />
+  <div className="viewer-container">
     <DAEModel
-      width="400"
-      height="400"
       scale={{ x: 0.5, y: 0.5, z: 0.5 }}
       src={alienware}
+      enableZoom={false}
     />
   </div>
 );
