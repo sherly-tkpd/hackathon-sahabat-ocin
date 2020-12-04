e5c1f3baae593ee9e3738d4df08a638cacaa0e8d
Fandika Okdiba <mr.parker0@gmail.com>
2020-12-04 15:34:22 +0700

    Fandika/adjust viewer 3d (#18)
    * delete eslint cache
    
    * adjust image position and rotation

--- src/components/Viewer3D/index.js
+++ src/components/Viewer3D/index.js
@@ -4,15 +4,28 @@ import "./index.css";
 
 const Viewer3D = ({ src, mtl, slug }) => {
   let texPath;
+  let rotation = { x: 0, y: 0, z: 0 };
+  let position = { x: 0, y: 0, z: 0 };
   if (slug === "television") {
-    texPath = "/model/television";
+    texPath = "/model/television/";
+    position.x = 10;
+    position.y = -80;
+    rotation.x = 29.83;
   } else if (slug === "vas") {
-    texPath = "/model/vase";
+    texPath = "/model/vase/";
+    position.y = -3.5;
   }
 
   return (
     <div className="viewer-container">
-      <MTLModel src={src} mtl={mtl} texPath={texPath} enableZoom={false} />
+      <MTLModel
+        src={src}
+        mtl={mtl}
+        texPath={texPath}
+        enableZoom={false}
+        position={position}
+        rotation={rotation}
+      />
     </div>
   );
 };
