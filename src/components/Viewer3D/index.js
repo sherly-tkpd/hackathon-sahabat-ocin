f220522adc848a3a3da9e88c550e9b4b1c5cb87d
Fandika Okdiba <mr.parker0@gmail.com>
2020-12-04 11:01:32 +0700

    Fandika/rebase (#15)
    * integrate products with viewer
    
    * merge with andy changes

--- src/components/Viewer3D/index.js
+++ src/components/Viewer3D/index.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { DAEModel } from "react-3d-viewer";
-import alienware from "../../assets/laptop/alienware.dae";
+import { MTLModel } from "react-3d-viewer";
 import "./index.css";
 
-const Viewer3D = () => (
-  <div className="viewer-container">
-    <DAEModel
-      scale={{ x: 0.5, y: 0.5, z: 0.5 }}
-      src={alienware}
-      enableZoom={false}
-    />
-  </div>
-);
+const Viewer3D = ({ src, mtl, slug }) => {
+  let texPath;
+  if (slug === "television") {
+    texPath = "/model/television";
+  } else if (slug === "vas") {
+    texPath = "/model/vase";
+  }
+
+  return (
+    <div className="viewer-container">
+      <MTLModel src={src} mtl={mtl} texPath={texPath} enableZoom={false} />
+    </div>
+  );
+};
 
 export default Viewer3D;
--- src/routes/ProductDetail/index.js
+++ src/routes/ProductDetail/index.js
@@ -8,8 +8,8 @@ import products from "../../constants/products";
 
 import Modal from "react-modal";
 import Viewer3D from "../../components/Viewer3D";
-import WebXR from './components/WebXR';
-import VirtualMakeUp from '../../components/VirtualMakeUp';
+import WebXR from "./components/WebXR";
+import VirtualMakeUp from "../../components/VirtualMakeUp";
 
 const customStyles = {
   content: {
@@ -22,11 +22,11 @@ const customStyles = {
   },
 };
 const imgThumbnailStyle = {
-  maxWidth: '100%',
-  maxHeight: '100%',
-  display: 'block',
-  margin: '0 auto',
-}
+  maxWidth: "100%",
+  maxHeight: "100%",
+  display: "block",
+  margin: "0 auto",
+};
 
 const ProductList = () => {
   const { slug = "" } = useParams();
@@ -52,7 +52,10 @@ const ProductList = () => {
           {product.shades.map((shade) => (
             <li>
               <a href="#">
-                <i style={{color: shade.trueTone}} class="zmdi zmdi-circle"></i>
+                <i
+                  style={{ color: shade.trueTone }}
+                  class="zmdi zmdi-circle"
+                ></i>
               </a>
             </li>
           ))}
@@ -63,39 +66,51 @@ const ProductList = () => {
 
   const renderProductSmallImages = () => (
     <ul class="product__small__images" role="tablist">
-      {product.images.map((imgUrl, index)=> {
-        let imgHref = `#img-tab-${index+1}`;
+      {product.images.map((imgUrl, index) => {
+        let imgHref = `#img-tab-${index + 1}`;
         return (
           <li role="presentation" class="pot-small-img" key={index}>
-              <a href={imgHref} role="tab" data-toggle="tab" style={{width:'120px', height:'140px'}}>
-                  <img src={imgUrl} alt="small-image" style={imgThumbnailStyle}/>
-              </a>
+            <a
+              href={imgHref}
+              role="tab"
+              data-toggle="tab"
+              style={{ width: "120px", height: "140px" }}
+            >
+              <img src={imgUrl} alt="small-image" style={imgThumbnailStyle} />
+            </a>
           </li>
         );
       })}
     </ul>
-  )
+  );
 
   const renderProductBigImages = () => (
     <div class="product__big__images">
       <div class="portfolio-full-image tab-content">
-          {product.images.map((imgUrl, index) => (
-              <div role="tabpanel" class={`tab-pane fade in ${index === 0? "active": ""} product-video-position`} id={`img-tab-${index+1}`}>
-                  <img src={imgUrl} alt="full-image"/>
-                  {product.objectModel && render360ViewButton()}
-              </div>
-          ))}
+        {product.images.map((imgUrl, index) => (
+          <div
+            role="tabpanel"
+            class={`tab-pane fade in ${
+              index === 0 ? "active" : ""
+            } product-video-position`}
+            id={`img-tab-${index + 1}`}
+          >
+            <img src={imgUrl} alt="full-image" />
+            {product.objectModel && render360ViewButton()}
+          </div>
+        ))}
       </div>
-    </div> )
+    </div>
+  );
 
   // TODO: Andy -- popup for try on face functionality
   const handleTryMakeUp = () => {
-    setShowMakeup(true)
+    setShowMakeup(true);
   };
 
   // TODO: Ocin -- popup for place object in room functionality
   const handlePlaceInRoom = () => {
-    if(product.objectURL !== ''){
+    if (product.objectURL !== "") {
       new WebXR(product.objectURL);
     }
   };
@@ -103,77 +118,122 @@ const ProductList = () => {
   return (
     <Fragment>
       <Header title="Product Details" />
-
-        <section class="htc__product__details pt--120 pb--100 bg__white">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
-                        <div class="product__details__container">
-                            {renderProductSmallImages()}
-                            {renderProductBigImages()}
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12 smt-30 xmt-30">
-                        <div class="htc__product__details__inner">
-                            <div class="pro__detl__title">
-                              <h2>{product.title}</h2>
-                            </div>
-                            <div class="pro__dtl__rating">
-                                <ul class="pro__rating">
-                                    <li><span class="ti-star"></span></li>
-                                    <li><span class="ti-star"></span></li>
-                                    <li><span class="ti-star"></span></li>
-                                    <li><span class="ti-star"></span></li>
-                                    <li><span class="ti-star"></span></li>
-                                </ul>
-                                <span class="rat__qun">(Based on 0 Ratings)</span>
-                            </div>
-                            <div class="pro__details">
-                              <p>{product.description}</p>
-                            </div>
-                            <ul class="pro__dtl__prize">
-                                <li class="old__prize">{product.price*1.25}</li>
-                                <li>{product.price}</li>
-                            </ul>
-                            {product.faceModel && renderColorOptions()}
-                            <div class="product-action-wrap">
-                                <div class="prodict-statas"><span>Quantity :</span></div>
-                                <div class="product-quantity">
-                                    <form id='myform' method='POST' action='#'>
-                                        <div class="product-quantity">
-                                            <div class="cart-plus-minus">
-                                                <input class="cart-plus-minus-box" type="text" name="qtybutton" value="02"/>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                            <ul class="pro__dtl__btn">
-                                {product.faceModel ? 
-                                  <li class="buy__now__btn" onClick={handleTryMakeUp}><a href="#">TRY ON</a></li> : 
-                                  <li class="buy__now__btn" onClick={handlePlaceInRoom}><a href="#">PLACE IN ROOM</a></li>
-                                }
-                                <li><a href="#"><span class="ti-heart"></span></a></li>
-                            </ul>
-                            <div class="pro__social__share">
-                                <h2>Share :</h2>
-                                <ul class="pro__soaial__link">
-                                    <li><a href="#"><i class="zmdi zmdi-twitter"></i></a></li>
-                                    <li><a href="#"><i class="zmdi zmdi-instagram"></i></a></li>
-                                    <li><a href="#"><i class="zmdi zmdi-facebook"></i></a></li>
-                                    <li><a href="#"><i class="zmdi zmdi-google-plus"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+      <section class="htc__product__details pt--120 pb--100 bg__white">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
+              <div class="product__details__container">
+                {renderProductSmallImages()}
+                {renderProductBigImages()}
+              </div>
             </div>
-        </section>
+            <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12 smt-30 xmt-30">
+              <div class="htc__product__details__inner">
+                <div class="pro__detl__title">
+                  <h2>{product.title}</h2>
+                </div>
+                <div class="pro__dtl__rating">
+                  <ul class="pro__rating">
+                    <li>
+                      <span class="ti-star"></span>
+                    </li>
+                    <li>
+                      <span class="ti-star"></span>
+                    </li>
+                    <li>
+                      <span class="ti-star"></span>
+                    </li>
+                    <li>
+                      <span class="ti-star"></span>
+                    </li>
+                    <li>
+                      <span class="ti-star"></span>
+                    </li>
+                  </ul>
+                  <span class="rat__qun">(Based on 0 Ratings)</span>
+                </div>
+                <div class="pro__details">
+                  <p>{product.description}</p>
+                </div>
+                <ul class="pro__dtl__prize">
+                  <li class="old__prize">{product.price * 1.25}</li>
+                  <li>{product.price}</li>
+                </ul>
+                {product.faceModel && renderColorOptions()}
+                <div class="product-action-wrap">
+                  <div class="prodict-statas">
+                    <span>Quantity :</span>
+                  </div>
+                  <div class="product-quantity">
+                    <form id="myform" method="POST" action="#">
+                      <div class="product-quantity">
+                        <div class="cart-plus-minus">
+                          <input
+                            class="cart-plus-minus-box"
+                            type="text"
+                            name="qtybutton"
+                            value="02"
+                          />
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+                <ul class="pro__dtl__btn">
+                  {product.faceModel ? (
+                    <li class="buy__now__btn" onClick={handleTryMakeUp()}>
+                      <a href="#">TRY ON</a>
+                    </li>
+                  ) : (
+                    <li class="buy__now__btn" onClick={handlePlaceInRoom()}>
+                      <a href="#">PLACE IN ROOM</a>
+                    </li>
+                  )}
+                  <li>
+                    <a href="#">
+                      <span class="ti-heart"></span>
+                    </a>
+                  </li>
+                </ul>
+                <div class="pro__social__share">
+                  <h2>Share :</h2>
+                  <ul class="pro__soaial__link">
+                    <li>
+                      <a href="#">
+                        <i class="zmdi zmdi-twitter"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i class="zmdi zmdi-instagram"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i class="zmdi zmdi-facebook"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i class="zmdi zmdi-google-plus"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       <div id="stabilization"></div>
 
       {showModal && (
         <Modal isOpen={showModal} style={customStyles}>
-          <Viewer3D setShowModal={setShowModal} />
+          <Viewer3D
+            src={product.objectURL[0]}
+            mtl={product.objectURL[1]}
+            slug={slug}
+          />
           <button
             style={{ marginTop: 10 }}
             onClick={() => setShowModal((val) => !val)}
@@ -184,14 +244,16 @@ const ProductList = () => {
       )}
 
       <Footer />
-      {showMakeup &&
+      {showMakeup && (
         <VirtualMakeUp
-          lips={product.facePart === 'lip' ? product.shades : []}
-          shadow={product.facePart === 'eye' ? product.shades : []}
-          skins={product.facePart === 'face' ? product.shades : []}
-          onClose={()=>{setShowMakeup(false)}}
+          lips={product.facePart === "lip" ? product.shades : []}
+          shadow={product.facePart === "eye" ? product.shades : []}
+          skins={product.facePart === "face" ? product.shades : []}
+          onClose={() => {
+            setShowMakeup(false);
+          }}
         />
-      } 
+      )}
     </Fragment>
   );
 };
